46c223e1e0a1aaa0244b177ac0acc421a61d0415
Habetyes <Habetyes@gmail.com>
2019-04-04 20:54:25 -0700

    Original DataFrame

---
+++ Code.py
@@ -0,0 +1,21 @@
+import requests
+import json
+import pandas as pd
+from pprint import pprint
+
+states = ["Alabama", "Alaska", "Arizona","Arkansas","California","Colorado","Connecticut","Delaware","District of Columbia","Florida","Georgia",
+          "Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts",
+          "Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey",
+          "New Mexico","New York","North Carolina","North Dakota","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island",
+          "South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin",
+          "Wyoming"
+]
+
+df = pd.DataFrame(data={"States": states})
+df["Median Income"] = ""
+df["Home Value"] = ""
+df["Bachelors Degree"] = ""
+df["Population"] = ""
+df["Divorce"] = ""
+
+print(df)
