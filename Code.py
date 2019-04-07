169a394b34ff947256f3a119cc4e9d5747fb1a1c
Habetyes <Habetyes@gmail.com>
2019-04-07 01:49:53 -0700

    Gmaps

--- Code.py
+++ Code.py
@@ -4,11 +4,25 @@ import pandas as pd
 from pprint import pprint
 from census import Census
 from us import states
+import gmaps
+
 
 #Census API Key
 from config import api_key
 c = Census(api_key, year=2017)
 c2= Census(api_key, year=2011)
+
+# Gmaps
+from config import gkey
+import gmaps.geojson_geometries
+gmaps.configure(api_key=gkey)
+
+# Matplotlib colors
+import matplotlib
+from matplotlib import cm
+from matplotlib.cm import viridis
+from matplotlib.cm import coolwarm
+from matplotlib.colors import to_hex
 
 states = ["Alabama", "Alaska", "Arizona","Arkansas","California","Colorado","Connecticut","Delaware","District of Columbia","Florida","Georgia",
           "Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts",
@@ -136,3 +150,52 @@ census_merged["Unemployment Score"]=census_merged["Unemployment Change"].rank(as
 census_merged=census_merged[["State","Population Change", "Population Score", "Education Change","Education Score", "Unemployment Change", "Unemployment Score"]]
 census_merged["Composite Score"]=census_merged["Population Score"]+census_merged["Education Score"]+census_merged["Unemployment Score"]
 census_merged
+
+# Begin Building Gmaps states layer
+states_geojson = gmaps.geojson_geometries.load_geometry('us-states')
+fig=gmaps.figure()
+unemployment_layer = gmaps.geojson_layer(states_geojson)
+fig.add_layer(unemployment_layer)
+
+# Transform dataframe into dictionary in order to allow gmaps to iterate through
+censusdict = census_merged[["State", "Unemployment Change"]]
+censusdict.set_index("State", inplace=True)
+censusdict2 = censusdict.to_dict()
+
+# Scale the values
+# Note: When negative values are better, use the inverse function for min/max
+min_unemployment = max(census_merged["Unemployment Change"])
+max_unemployment = min(census_merged["Unemployment Change"])
+unemployment_range = min_unemployment - max_unemployment
+
+# Create a function to transform the values into a color
+def calculate_color(unemployment):
+#     Make the unemployment change a number between 0 and 1
+    normalized_unemployment = (unemployment - max_unemployment)/unemployment_range
+#     invert unemployment to make worse performers blue, high performers red
+    inverse_unemployment = 1.0 - normalized_unemployment
+#     transform the unemployment numbers to a matplotlib color
+    mpl_color = coolwarm(inverse_unemployment)
+#     transform from a matplotlib color to a valid CSS color
+    gmaps_color = to_hex(mpl_color, keep_alpha=False)
+    
+    return gmaps_color
+    
+# Loop through each state to build an array of colors
+colors = []
+for feature in states_geojson['features']:
+    state_name = feature["properties"]["NAME"]
+    try:
+        unemployment = censusdict2["Unemployment Change"][state_name]
+        color = calculate_color(unemployment)
+    except (KeyError):
+        color = (0,0,0,0.3)
+    colors.append(color)
+
+# Build map
+fig=gmaps.figure()
+unemployment_layer = gmaps.geojson_layer(states_geojson,fill_color=colors,stroke_color=colors,fill_opacity=0.8)
+fig.add_layer(unemployment_layer)
+# fig
+
+print("complete")
--- config.py
+++ config.py
@@ -1,1 +1,2 @@
 api_key = "24d01cf5880931bcc7141332be8817f9a90ee051"
+gkey = "AIzaSyAjJfeUiPcq6wFVCKcjjzc5-Y6A7dFkR98"
